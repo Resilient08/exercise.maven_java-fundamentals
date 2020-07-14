921c9c2f37081a8d6a53dc64bdbf81967f58adf9
Resilient08 <58645288+Resilient08@users.noreply.github.com>
2020-07-14 14:56:26 -0400

    finished Precicate Utilities

--- src/main/java/com/github/perscholas/java_fundamentals/PredicateUtilities.java
+++ src/main/java/com/github/perscholas/java_fundamentals/PredicateUtilities.java
@@ -20,7 +20,8 @@ public class PredicateUtilities {
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
-        return null;
+        if (x < y) { return true;
+        } else {return false;}
     }
 
     /**
@@ -29,8 +30,10 @@ public class PredicateUtilities {
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
+        if (x >= y) { return true;
+        } else {return false;}
     }
+
 
     /**
      * @param x
@@ -38,7 +41,8 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
+        if (x <= y) { return true;
+        } else {return false;}
     }
 
 
@@ -46,14 +50,14 @@ public class PredicateUtilities {
      * @return true
      */
     public Boolean returnTrue() {
-        return null;
+        return true;
     }
 
     /**
      * @return false
      */
     public Boolean returnFalse() {
-        return null;
+        return false;
     }
 
 }
